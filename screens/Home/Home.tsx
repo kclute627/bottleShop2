8f26fb3e9ecd8581875a81fb0706629842819ace
Kyle <you@example.com>
2021-09-28 07:32:27 -0500

    update

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,40 +1,92 @@
-import * as React from "react";
-import { Image, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, FlatList, TouchableOpacity } from "react-native";
+import AppLoading from "expo-app-loading";
 import styles from "./HomeStyles";
-
+import axios from "axios";
 import { Text, View } from "../../components/Themed";
 import { RootTabScreenProps } from "../../types";
 import { ScrollView } from "react-native-gesture-handler";
 
 type RenderItem = {
   item: {
-    title: string;
-    brewery: string;
-    photo: any;
-    price: number;
+    id: string;
+    image_id: string;
+    item_data: {
+      name: string;
+      variations: [
+        {
+          item_variation_data: {
+            price_money: number;
+          };
+        }
+      ];
+    };
   };
 };
-const whatsNew = [
-  {
-    title: "Dasiey Cutter",
-    brewery: "Half Acre",
-    photo: "../../assets/images/beer1.png",
-    price: 9.99,
-  },
-];
+type RenderImage = {
+  id: string;
+  image_id: string;
+  item: {
+    image_id: string;
+  };
+};
 
 export default function Home({ navigation }: RootTabScreenProps<"TabOne">) {
-  const renderItem = ({ item }: RenderItem): React.ReactElement => (
-    <View style={styles.item}>
-      <Text>{item.title}</Text>
-      <Text>${item.price}</Text>
-      <Image
-        source={require("../../assets/images/beer1.png")}
-        style={styles.itemPhoto}
-      />
-    </View>
-  );
+  const renderItem = ({ item }: RenderItem): React.ReactElement => {
+    const url: any =
+      images.length > 0 &&
+      images.find((cur: RenderImage): boolean => cur.id === item.image_id);
 
+    const { amount }: any =
+      item.item_data.variations[0].item_variation_data.price_money;
+    return (
+      <View style={styles.item} key={item.id}>
+        <TouchableOpacity>
+        
+          <Image
+            source={{ uri: url.image_data.url }}
+            style={styles.itemPhoto}
+          />
+        </TouchableOpacity>
+        <Text numberOfLines={1} style={styles.text}>
+          {item.item_data.name}
+        </Text>
+        <Text style={styles.price}>
+          ${amount.toString().slice(0, -2)}.{amount.toString().slice(-2)}
+        </Text>
+      </View>
+    );
+  };
+
+  const [images, setImages] = useState([]);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getImages();
+    getItems();
+  }, []);
+
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer EAAAEH4RvSStHqoTTLqBmAEa4tuvJg5qQbN_PtOpfDOnBEprrZHG9iqrnMU-bhwP",
+    },
+  };
+  const getItems = async () => {
+    const { data } = await axios.get(
+      "https://connect.squareupsandbox.com/v2/catalog/list?types=ITEM",
+      config
+    );
+    setItems(data.objects);
+  };
+  const getImages = async () => {
+    const { data } = await axios.get(
+      "https://connect.squareupsandbox.com/v2/catalog/list?types=IMAGE",
+      config
+    );
+
+    setImages(data.objects);
+  };
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.homeTop}>
@@ -45,12 +97,21 @@ export default function Home({ navigation }: RootTabScreenProps<"TabOne">) {
         <Text style={[styles.homeTopText]}></Text>
       </View>
       <View style={styles.new}>
-        <Text style={styles.title}>Whats New</Text>
-        <FlatList
-          data={whatsNew}
-          keyExtractor={(item) => item.title}
-          renderItem={renderItem}
-        />
+        <View style={styles.newTop}>
+          <Text style={styles.title}>Whats New</Text>
+          <Text style={styles.title}>See All</Text>
+        </View>
+
+        {images.length <= 1 || items.length <= 1 ? (
+          <AppLoading />
+        ) : (
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+            horizontal
+          />
+        )}
       </View>
     </ScrollView>
   );
--- screens/Home/HomeStyles.ts
+++ screens/Home/HomeStyles.ts
@@ -20,14 +20,14 @@ const styles = StyleSheet.create({
     bottom: 70,
     left: 45,
     zIndex: 223,
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     fontSize: 45,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    color: '#000'
+    color: "#000",
   },
   separator: {
     marginVertical: 30,
@@ -35,18 +35,41 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   new: {
-      marginVertical: 45,
-      marginHorizontal: 10,
-      backgroundColor: '#fff',
+    marginVertical: 45,
+    marginHorizontal: 10,
+    backgroundColor: "#fff",
+    width: "95%",
+    height: 160,
+    position: "relative",
+    
+    
+  },
+  newTop: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   item: {
-      height: 55,
-      width: 100,
+    height: 55,
+    width: 120,
+    paddingTop: 20,
+    paddingLeft: 10,
+    alignItems: 'center'
+    
   },
-  itemPhoto:{
-      height: 145,
-      width: 145,
-  }
+  text:{
+    textAlign: 'center',
+    marginTop: 2,
+    
+    width: '75%'
+  },
+  price: {
+    fontWeight: 'bold'
+  },
+  itemPhoto: {
+    height: 65,
+    width: 65,
+    borderRadius: 200,
+  },
 });
 
 export default styles;
