addda957993ca15e8f015a24932f4bb2c9827363
Kyle C <kyclutter@gmail.com>
2021-09-29 09:04:16 -0500

    updates

---
+++ screens/ItemScreen/ItemScreen-styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+image: {
+    height: 275,
+    width: '100%'
+},
+container:{
+    flex: 1
+}
+})
--- screens/ItemScreen/Itemscreen.tsx
+++ screens/ItemScreen/Itemscreen.tsx
@@ -1,14 +1,27 @@
-import React, { ReactElement } from "react";
-
+import React, { ReactElement, useState } from "react";
+import { styles } from "./ItemScreen-styles";
+import AppLoading from "expo-app-loading";
 import { Text, View, Image } from "react-native";
 
 export default function Itemscreen({ route }: any): ReactElement {
+  const [loading, setLoading] = useState(false);
+
   console.log(route.params.itemInformation.item);
   const { itemInformation, image } = route.params;
   return (
-    <View>
-      <Image source={{ uri: image }} />
-      <Text> textInComponent </Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: image }}
+        style={styles.image}
+        onLoad={() => setLoading(true)}
+      />
+      {loading ? (
+        <>
+          <Text> textInComponent </Text>
+        </>
+      ) : (
+        <AppLoading />
+      )}
     </View>
   );
 }
